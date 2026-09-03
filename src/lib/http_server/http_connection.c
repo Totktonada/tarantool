8fcf5108f05c5dc8947566feefa9f60b41c515b0
Alexander Turenko <alexander.turenko@tarantool.org>
2026-09-03 08:10:40 +0300

    WIP: http connection stub

---
+++ src/lib/http_server/http_connection.c
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2026, Tarantool AUTHORS, please see AUTHORS file.
+ */
+#include "http_connection.h"
+
+
---
+++ src/lib/http_server/http_connection.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2026, Tarantool AUTHORS, please see AUTHORS file.
+ */
+#pragma once
+
+#include "core/iostream.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct http_connection {
+};
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
